54b3fe03445fda64acec43bbd057e468b0d90bf2
monicaep <mep823@gmail.com>
2019-04-04 07:53:02 -0700

    Node Core Libraries: complete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const requestHandler = (request, response) => {
   fs.writeFile('hello-word.txt', 'Hello to this great world', 'utf8', (err) => {
     if (err) throw err;
-    response.end(`Handling a request on port ${port}`)
+    response.end(`Handling a request on port ${port}`);
   });
 };
 
